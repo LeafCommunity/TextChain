74fc9a7122e968e64f024e0669ef659e03e94bcd
RezzedUp <RezzedUp@users.noreply.github.com>
2021-01-18 06:48:23 -0600

    Update to Adventure API 4.4.0. Misc pom.xml edits. Add example command.

--- examples/example-bukkit/src/main/java/community/leaf/textchain/examples/bukkit/TextChainExamplePlugin.java
+++ examples/example-bukkit/src/main/java/community/leaf/textchain/examples/bukkit/TextChainExamplePlugin.java
@@ -2,7 +2,12 @@ package community.leaf.textchain.examples.bukkit;
 
 import community.leaf.textchain.adventure.TextChain;
 import net.kyori.adventure.platform.bukkit.BukkitAudiences;
+import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TextChainExamplePlugin extends JavaPlugin
@@ -17,4 +22,25 @@ public class TextChainExamplePlugin extends JavaPlugin
     }
     
     public BukkitAudiences getAudiences() { return audiences; }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
+    {
+        TextComponent component = TextChain.empty()
+            .then("Hello ")
+                .color(NamedTextColor.GOLD)
+                .bold()
+                .extra(inherits -> inherits.then("friend!").italic())
+            .then(" ")
+            .then("How are you?")
+                .color(TextColor.color(0xFF0000))
+                .tooltip("Click here to respond with \"pretty good\"")
+                .suggest("pretty good")
+            .asComponent();
+        
+        audiences.sender(sender).sendMessage(component);
+        sender.sendMessage("As JSON: " + GsonComponentSerializer.gson().serialize(component));
+        
+        return true;
+    }
 }
