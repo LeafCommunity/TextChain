79429dadeaf9db2b5859fa77888029405428e2e5
RezzedUp <RezzedUp@users.noreply.github.com>
2021-03-17 16:59:43 -0500

    Get item translation keys properly.

--- textchain-bukkit/src/main/java/community/leaf/textchain/bukkit/ShowItems.java
+++ textchain-bukkit/src/main/java/community/leaf/textchain/bukkit/ShowItems.java
@@ -16,7 +16,6 @@ import net.md_5.bungee.chat.TranslationRegistry;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import pl.tlinkowski.annotation.basic.NullOr;
 
 import java.lang.invoke.MethodHandle;
 import java.lang.reflect.Field;
@@ -27,9 +26,7 @@ public class ShowItems
 {
     private ShowItems() { throw new UnsupportedOperationException(); }
     
-    //
-    //  These classes should exist as if importing them directly.
-    //
+    // These classes should exist as if importing them directly.
     
     static final Class<?> CRAFT_ITEM_STACK = ServerReflection.requireCraftClass("inventory.CraftItemStack");
     
@@ -37,16 +34,16 @@ public class ShowItems
     
     static final Class<?> NMS_ENUM_ITEM_RARITY = ServerReflection.requireNmsClass("EnumItemRarity");
     
+    static final Class<?> NMS_BLOCK = ServerReflection.requireNmsClass("Block");
+    
     static final Class<?> NMS_ITEM = ServerReflection.requireNmsClass("Item");
     
     static final Class<?> NMS_ITEM_STACK = ServerReflection.requireNmsClass("ItemStack");
     
     static final Class<?> NMS_NBT_TAG_COMPOUND = ServerReflection.requireNmsClass("NBTTagCompound");
     
-    //
-    //  Methods and fields that may or not exist - most likely they do in fact exist.
-    //  But let's not throw anything until called (like standard java behavior).
-    //
+    // Methods and fields that may or not exist, but they most likely do in fact exist.
+    // But let's not throw anything until called (like standard java behavior).
     
     static final ThrowsOr<MethodHandle> AS_NMS_COPY =
         ServerReflection.requireStaticMethod(CRAFT_ITEM_STACK, "asNMSCopy", NMS_ITEM_STACK, ItemStack.class);
@@ -54,21 +51,25 @@ public class ShowItems
     static final ThrowsOr<MethodHandle> GET_OR_CREATE_TAG =
         ServerReflection.requireMethod(NMS_ITEM_STACK, "getOrCreateTag", NMS_NBT_TAG_COMPOUND);
     
+    static final ThrowsOr<MethodHandle> GET_BLOCK_BY_MATERIAL =
+        ServerReflection.requireStaticMethod(CRAFT_MAGIC_NUMBERS, "getBlock", NMS_BLOCK, Material.class);
+    
+    static final ThrowsOr<MethodHandle> GET_BLOCK_NAME =
+        ServerReflection.requireMethod(NMS_BLOCK, "i", String.class);
+    
     static final ThrowsOr<MethodHandle> GET_ITEM_BY_MATERIAL =
         ServerReflection.requireStaticMethod(CRAFT_MAGIC_NUMBERS, "getItem", NMS_ITEM, Material.class);
         
     static final ThrowsOr<MethodHandle> GET_ITEM_NAME =
         ServerReflection.requireMethod(NMS_ITEM, "getName", String.class);
     
-    static final ThrowsOr<MethodHandle> GET_ITEM_RARITY =
+    static final ThrowsOr<MethodHandle> GET_ITEM_RARITY_BY_ITEM_STACK =
         ServerReflection.requireMethod(NMS_ITEM, "i", NMS_ENUM_ITEM_RARITY, NMS_ITEM_STACK);
     
-    static final ThrowsOr<Field> NMS_ITEM_RARITY =
+    static final ThrowsOr<Field> NMS_ITEM_RARITY_FIELD =
         ServerReflection.requireField(NMS_ITEM, "a", NMS_ENUM_ITEM_RARITY);
     
-    //
-    //
-    //
+    // Utility methods.
     
     public static BinaryTagHolder nbt(ItemStack item)
     {
@@ -89,22 +90,22 @@ public class ShowItems
         return HoverEvent.showItem(showItem);
     }
     
-    private static String fallbackTranslationKey(Material material)
-    {
-        return ((material.isBlock()) ? "block" : "item") + ".minecraft." + material.name().toLowerCase();
-    }
-    
     public static String asTranslationKey(Material material)
     {
         Objects.requireNonNull(material, "material");
         
         try
         {
-            @NullOr Object nmsItem = GET_ITEM_BY_MATERIAL.getOrThrow().invoke(material); // can return null apparently?
-            
-            return (nmsItem != null)
-                ? String.valueOf(GET_ITEM_NAME.getOrThrow().invoke(nmsItem))
-                : fallbackTranslationKey(material);
+            if (material.isBlock())
+            {
+                Object nmsBlock = GET_BLOCK_BY_MATERIAL.getOrThrow().invoke(material);
+                return String.valueOf(GET_BLOCK_NAME.getOrThrow().invoke(nmsBlock));
+            }
+            else
+            {
+                Object nmsItem = GET_ITEM_BY_MATERIAL.getOrThrow().invoke(material);
+                return String.valueOf(GET_ITEM_NAME.getOrThrow().invoke(nmsItem));
+            }
         }
         catch (Throwable throwable) { throw new RuntimeException(throwable); }
     }
@@ -174,7 +175,7 @@ public class ShowItems
         try
         {
             Object nmsItem = GET_ITEM_BY_MATERIAL.getOrThrow().invoke(material);
-            Object nmsRarity = NMS_ITEM_RARITY.getOrThrow().get(nmsItem);
+            Object nmsRarity = NMS_ITEM_RARITY_FIELD.getOrThrow().get(nmsItem);
             return ItemRarity.resolveByName(String.valueOf(nmsRarity)).orElse(ItemRarity.COMMON);
         }
         catch (Throwable throwable) { throw new RuntimeException(throwable); }
@@ -191,7 +192,7 @@ public class ShowItems
         {
             Object nmsItem = GET_ITEM_BY_MATERIAL.getOrThrow().invoke(material);
             Object nmsItemStack = AS_NMS_COPY.getOrThrow().invoke(item);
-            Object nmsRarity = GET_ITEM_RARITY.getOrThrow().invoke(nmsItem, nmsItemStack);
+            Object nmsRarity = GET_ITEM_RARITY_BY_ITEM_STACK.getOrThrow().invoke(nmsItem, nmsItemStack);
             return ItemRarity.resolveByName(String.valueOf(nmsRarity)).orElse(ItemRarity.COMMON);
         }
         catch (Throwable throwable) { throw new RuntimeException(throwable); }
