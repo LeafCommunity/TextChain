a9b8349863be1cb81509c5a30d3bfd32da2504d5
RezzedUp <RezzedUp@users.noreply.github.com>
2024-01-01 16:45:43 -0600

    Update license headers (2024).

--- textchain-platforms/src/main/java/community/leaf/textchain/platforms/adapters/ItemMetaAdapter.java
+++ textchain-platforms/src/main/java/community/leaf/textchain/platforms/adapters/ItemMetaAdapter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2021-2023, RezzedUp <https://github.com/LeafCommunity/TextChain>
+ * Copyright © 2021-2024, RezzedUp <https://github.com/LeafCommunity/TextChain>
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
